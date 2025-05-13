05c19c4c9be542e86bc1abce512947103ee25304
Foahh <160589889+Foahh@users.noreply.github.com>
2025-05-14 00:48:02 +0800

    reformat

--- hooklib/dll.c
+++ hooklib/dll.c
@@ -1,16 +1,15 @@
 /* This is general enough to break out into capnhook eventually.
    Don't introduce util/ dependencies here. */
 
-#include <windows.h>
+#include "hooklib/dll.h"
 
 #include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <windows.h>
 
 #include "hook/table.h"
-
-#include "hooklib/dll.h"
 
 struct dll_hook_reg {
     const wchar_t *name;
@@ -29,60 +28,66 @@ static HMODULE WINAPI hook_GetModuleHandleA(const char *name);
 static HMODULE WINAPI hook_GetModuleHandleW(const wchar_t *name);
 static HMODULE WINAPI hook_LoadLibraryA(const char *name);
 static HMODULE WINAPI hook_LoadLibraryW(const wchar_t *name);
-static HMODULE WINAPI hook_LoadLibraryExA(const char *name, HANDLE file, DWORD flags);
-static HMODULE WINAPI hook_LoadLibraryExW(const wchar_t *name, HANDLE file, DWORD flags);
+static HMODULE WINAPI hook_LoadLibraryExA(const char *name, HANDLE file,
+                                          DWORD flags);
+static HMODULE WINAPI hook_LoadLibraryExW(const wchar_t *name, HANDLE file,
+                                          DWORD flags);
 
 /* Link pointers */
 
-static BOOL (WINAPI *next_FreeLibrary)(HMODULE mod);
-static HMODULE (WINAPI *next_GetModuleHandleA)(const char *name);
-static HMODULE (WINAPI *next_GetModuleHandleW)(const wchar_t *name);
-static HMODULE (WINAPI *next_LoadLibraryA)(const char *name);
-static HMODULE (WINAPI *next_LoadLibraryW)(const wchar_t *name);
-static HMODULE (WINAPI *next_LoadLibraryExA)(const char *name, HANDLE file, DWORD flags);
-static HMODULE (WINAPI *next_LoadLibraryExW)(const wchar_t *name, HANDLE file, DWORD flags);
+static BOOL(WINAPI *next_FreeLibrary)(HMODULE mod);
+static HMODULE(WINAPI *next_GetModuleHandleA)(const char *name);
+static HMODULE(WINAPI *next_GetModuleHandleW)(const wchar_t *name);
+static HMODULE(WINAPI *next_LoadLibraryA)(const char *name);
+static HMODULE(WINAPI *next_LoadLibraryW)(const wchar_t *name);
+static HMODULE(WINAPI *next_LoadLibraryExA)(const char *name, HANDLE file,
+                                            DWORD flags);
+static HMODULE(WINAPI *next_LoadLibraryExW)(const wchar_t *name, HANDLE file,
+                                            DWORD flags);
 
 static const struct hook_symbol dll_loader_syms[] = {
     {
-        .name   = "FreeLibrary",
-        .patch  = hook_FreeLibrary,
-        .link   = (void **) &next_FreeLibrary,
-    }, {
-        .name   = "GetModuleHandleA",
-        .patch  = hook_GetModuleHandleA,
-        .link   = (void **) &next_GetModuleHandleA,
-    }, {
-        .name   = "GetModuleHandleW",
-        .patch  = hook_GetModuleHandleW,
-        .link   = (void **) &next_GetModuleHandleW,
-    }, {
-        .name   = "LoadLibraryA",
-        .patch  = hook_LoadLibraryA,
-        .link   = (void **) &next_LoadLibraryA,
-    }, {
-        .name   = "LoadLibraryW",
-        .patch  = hook_LoadLibraryW,
-        .link   = (void **) &next_LoadLibraryW,
-    }, {
-        .name   = "LoadLibraryExA",
-        .patch  = hook_LoadLibraryExA,
-        .link   = (void **) &next_LoadLibraryExA,
-    }, {
-        .name   = "LoadLibraryExW",
-        .patch  = hook_LoadLibraryExW,
-        .link   = (void **) &next_LoadLibraryExW,
-    }
-};
+        .name = "FreeLibrary",
+        .patch = hook_FreeLibrary,
+        .link = (void **)&next_FreeLibrary,
+    },
+    {
+        .name = "GetModuleHandleA",
+        .patch = hook_GetModuleHandleA,
+        .link = (void **)&next_GetModuleHandleA,
+    },
+    {
+        .name = "GetModuleHandleW",
+        .patch = hook_GetModuleHandleW,
+        .link = (void **)&next_GetModuleHandleW,
+    },
+    {
+        .name = "LoadLibraryA",
+        .patch = hook_LoadLibraryA,
+        .link = (void **)&next_LoadLibraryA,
+    },
+    {
+        .name = "LoadLibraryW",
+        .patch = hook_LoadLibraryW,
+        .link = (void **)&next_LoadLibraryW,
+    },
+    {
+        .name = "LoadLibraryExA",
+        .patch = hook_LoadLibraryExA,
+        .link = (void **)&next_LoadLibraryExA,
+    },
+    {
+        .name = "LoadLibraryExW",
+        .patch = hook_LoadLibraryExW,
+        .link = (void **)&next_LoadLibraryExW,
+    }};
 
 static bool dll_hook_initted;
 static CRITICAL_SECTION dll_hook_lock;
 static struct dll_hook_reg *dll_hook_list;
 static size_t dll_hook_count;
 
-HRESULT dll_hook_push(
-        HMODULE redir_mod,
-        const wchar_t *name)
-{
+HRESULT dll_hook_push(HMODULE redir_mod, const wchar_t *name) {
     struct dll_hook_reg *new_item;
     struct dll_hook_reg *new_mem;
     HRESULT hr;
@@ -93,9 +98,8 @@ HRESULT dll_hook_push(
 
     EnterCriticalSection(&dll_hook_lock);
 
-    new_mem = realloc(
-            dll_hook_list,
-            (dll_hook_count + 1) * sizeof(struct dll_hook_reg));
+    new_mem = realloc(dll_hook_list,
+                      (dll_hook_count + 1) * sizeof(struct dll_hook_reg));
 
     if (new_mem == NULL) {
         hr = E_OUTOFMEMORY;
@@ -117,8 +121,7 @@ end:
     return hr;
 }
 
-static void dll_hook_init(void)
-{
+static void dll_hook_init(void) {
     HMODULE kernel32;
 
     /* Init is not thread safe, because API hooking is not thread safe. */
@@ -140,20 +143,16 @@ static void dll_hook_init(void)
        before the hooks go in) */
 
     kernel32 = GetModuleHandleW(L"kernel32.dll");
-    next_LoadLibraryW = (void *) GetProcAddress(kernel32, "LoadLibraryW");
-    next_LoadLibraryExW = (void *) GetProcAddress(kernel32, "LoadLibraryExW");
+    next_LoadLibraryW = (void *)GetProcAddress(kernel32, "LoadLibraryW");
+    next_LoadLibraryExW = (void *)GetProcAddress(kernel32, "LoadLibraryExW");
 
     /* Now we can apply the hook table */
 
-    hook_table_apply(
-            NULL,
-            "kernel32.dll",
-            dll_loader_syms,
-            _countof(dll_loader_syms));
+    hook_table_apply(NULL, "kernel32.dll", dll_loader_syms,
+                     _countof(dll_loader_syms));
 }
 
-static HMODULE dll_hook_search_dll(const wchar_t *name)
-{
+static HMODULE dll_hook_search_dll(const wchar_t *name) {
     HMODULE result;
     size_t i;
 
@@ -161,7 +160,7 @@ static HMODULE dll_hook_search_dll(const wchar_t *name)
 
     EnterCriticalSection(&dll_hook_lock);
 
-    for (i = 0 ; i < dll_hook_count ; i++) {
+    for (i = 0; i < dll_hook_count; i++) {
         if (wcsicmp(name, dll_hook_list[i].name) == 0) {
             result = dll_hook_list[i].redir_mod;
 
@@ -174,15 +173,14 @@ static HMODULE dll_hook_search_dll(const wchar_t *name)
     return result;
 }
 
-static BOOL WINAPI hook_FreeLibrary(HMODULE mod)
-{
+static BOOL WINAPI hook_FreeLibrary(HMODULE mod) {
     bool match;
     size_t i;
 
     match = false;
     EnterCriticalSection(&dll_hook_lock);
 
-    for (i = 0 ; i < dll_hook_count ; i++) {
+    for (i = 0; i < dll_hook_count; i++) {
         if (mod == dll_hook_list[i].redir_mod) {
             match = true;
 
@@ -207,8 +205,7 @@ static BOOL WINAPI hook_FreeLibrary(HMODULE mod)
     return next_FreeLibrary(mod);
 }
 
-static HMODULE WINAPI hook_GetModuleHandleA(const char *name)
-{
+static HMODULE WINAPI hook_GetModuleHandleA(const char *name) {
     HMODULE result;
     wchar_t *name_w;
     size_t name_c;
@@ -233,8 +230,7 @@ static HMODULE WINAPI hook_GetModuleHandleA(const char *name)
     return result;
 }
 
-static HMODULE WINAPI hook_GetModuleHandleW(const wchar_t *name)
-{
+static HMODULE WINAPI hook_GetModuleHandleW(const wchar_t *name) {
     HMODULE result;
 
     if (name == NULL) {
@@ -252,8 +248,7 @@ static HMODULE WINAPI hook_GetModuleHandleW(const wchar_t *name)
     return result;
 }
 
-static HMODULE WINAPI hook_LoadLibraryA(const char *name)
-{
+static HMODULE WINAPI hook_LoadLibraryA(const char *name) {
     HMODULE result;
     wchar_t *name_w;
     size_t name_c;
@@ -280,8 +275,7 @@ static HMODULE WINAPI hook_LoadLibraryA(const char *name)
     return result;
 }
 
-static HMODULE WINAPI hook_LoadLibraryW(const wchar_t *name)
-{
+static HMODULE WINAPI hook_LoadLibraryW(const wchar_t *name) {
     HMODULE result;
 
     if (name == NULL) {
@@ -301,8 +295,8 @@ static HMODULE WINAPI hook_LoadLibraryW(const wchar_t *name)
     return result;
 }
 
-static HMODULE WINAPI hook_LoadLibraryExA(const char *name, HANDLE file, DWORD flags)
-{
+static HMODULE WINAPI hook_LoadLibraryExA(const char *name, HANDLE file,
+                                          DWORD flags) {
     HMODULE result;
     wchar_t *name_w;
     size_t name_c;
@@ -329,8 +323,8 @@ static HMODULE WINAPI hook_LoadLibraryExA(const char *name, HANDLE file, DWORD f
     return result;
 }
 
-static HMODULE WINAPI hook_LoadLibraryExW(const wchar_t *name, HANDLE file, DWORD flags)
-{
+static HMODULE WINAPI hook_LoadLibraryExW(const wchar_t *name, HANDLE file,
+                                          DWORD flags) {
     HMODULE result;
 
     if (name == NULL) {
--- hooklib/dll.h
+++ hooklib/dll.h
@@ -1,10 +1,7 @@
 #pragma once
 
-#include <windows.h>
-
 #include <stddef.h>
 #include <stdint.h>
+#include <windows.h>
 
-HRESULT dll_hook_push(
-        HMODULE redir_mod,
-        const wchar_t *name);
+HRESULT dll_hook_push(HMODULE redir_mod, const wchar_t *name);
